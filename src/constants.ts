70bfdf7790d9bd921a246af6cadb60680cad5fe4
dave-aa <dave@artificial.agency>
2025-08-22 10:06:22 -0600

    convert black color to constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,8 @@
 export class Constants {
     public static readonly NODE_FONT = "12px sans-serif";
     public static readonly NODE_HEADER_FONT = "bold 12px sans-serif";
-    public static readonly NODE_BACKGROUND_COLOR = "rgba(15,15,15,0.6)";
+    public static readonly BLACK = "rgba(15,15,15,0.6)";
+    public static readonly NODE_BACKGROUND_COLOR = Constants.BLACK;
     public static readonly NODE_TEXT_COLOR = "rgb(240,240,240)";
     public static readonly NODE_SUBTITLE_COLOR = "rgb(170,170,170)";
 
